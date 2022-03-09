2877019a1f351b743f54b338203ea95212559ebd
Bread God <Vipcube@users.noreply.github.com>
2022-03-10 00:32:53 +0800

    Add test of record type model.

--- OpenDataFeign-WRA-FHY/src/main/java/org/vipcube/openfeign/wra/fhy/v1/dto/record/BasicRealTimeRecord.java
+++ OpenDataFeign-WRA-FHY/src/main/java/org/vipcube/openfeign/wra/fhy/v1/dto/record/BasicRealTimeRecord.java
@@ -1,11 +1,24 @@
 package org.vipcube.openfeign.wra.fhy.v1.dto.record;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
+import java.io.IOException;
+import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 
 /**
  * The parent model of real-time record.
@@ -20,6 +33,26 @@ public abstract class BasicRealTimeRecord {
 	@JsonProperty( "StationNo" )
 	private String stationId;
 
+	@JsonSerialize( using = TimeSerializer.class )
+	@JsonDeserialize( using = TimeDeserializer.class )
 	@JsonProperty( "Time" )
 	private OffsetDateTime time;
+
+	private static class TimeSerializer extends JsonSerializer<OffsetDateTime> {
+		@Override
+		public void serialize( OffsetDateTime offsetDateTime, JsonGenerator jsonGenerator,
+				SerializerProvider serializerProvider ) throws IOException {
+			String time = offsetDateTime.format( DateTimeFormatter.ofPattern( "yyyy-MM-dd'T'HH:mm:ss" ) );
+			jsonGenerator.writeString( time );
+		}
+	}
+
+	private static class TimeDeserializer extends JsonDeserializer<OffsetDateTime> {
+		@Override
+		public OffsetDateTime deserialize( JsonParser jsonParser, DeserializationContext deserializationContext )
+				throws IOException, JsonProcessingException {
+			LocalDateTime localDateTime = LocalDateTime.parse( jsonParser.getText() );
+			return OffsetDateTime.of( localDateTime, ZoneOffset.ofHours( 8 ) );
+		}
+	}
 }
---
+++ OpenDataFeign-WRA-FHY/src/test/java/org/vipcube/openfeign/wra/fhy/v1/dto/record/ReservoirRealTimeRecordModelTest.java
@@ -0,0 +1,40 @@
+package org.vipcube.openfeign.wra.fhy.v1.dto.record;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.vipcube.openfeign.wra.fhy.TestApplication;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+
+@SpringBootTest( classes = { TestApplication.class } )
+public class ReservoirRealTimeRecordModelTest {
+	@Autowired
+	private ObjectMapper mapper;
+
+	@Test
+	public void test() throws IOException {
+		// ShouldDeserialize
+		String jsonString = Files.readString( Paths.get( "src/test/resources/json/real-time-record_v1_reservoir.json" ) );
+		JsonNode jsonElement = this.mapper.readTree( jsonString );
+
+		ReservoirRealTimeRecord record = this.mapper.readValue( jsonString, ReservoirRealTimeRecord.class );
+		Assertions.assertAll( "BasicTypeCheck",
+				() -> Assertions.assertEquals( jsonElement.get( "StationNo" ).textValue(), record.getStationId() ),
+				() -> Assertions.assertEquals( LocalDateTime.parse( jsonElement.get( "Time" ).textValue() ), record.getTime().toLocalDateTime() )
+		);
+
+		Assertions.assertAll( "ActualTypeCheck",
+				() -> Assertions.assertEquals( jsonElement.get( "WaterHeight" ).decimalValue(), record.getWaterHeight() ),
+				() -> Assertions.assertEquals( jsonElement.get( "EffectiveStorage" ).decimalValue(), record.getEffectiveStorage() ),
+				() -> Assertions.assertEquals( jsonElement.get( "PercentageOfStorage" ).decimalValue(), record.getPercentageOfStorage() ),
+				() -> Assertions.assertEquals( jsonElement.get( "Status" ).intValue(), record.getStatus() )
+		);
+	}
+}
